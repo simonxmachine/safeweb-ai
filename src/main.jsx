d8523174f0a2b5e07f68d374e39337747b3a083f
Simon Leong <simonremgmt@gmail.com>
2024-02-07 12:27:24 -0500

    [refactor] updated analytics config

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { inject } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
  
-inject();
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Analytics />
     <App />
     </BrowserRouter>
   </React.StrictMode>,
